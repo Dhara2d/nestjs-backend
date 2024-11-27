b93ba83335033707b8f1895e1b28f9d749df1aee
dhara2d <dharanidharan10057@gmail.com>
2024-11-27 15:51:12 +0530

    first change for docker

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World';
+    return 'Hello World From dockerized image running on EC2 machine';
   }
 
   getHelp(): string {
